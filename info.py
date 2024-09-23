b8642c0665ec9e680d800f84a43ce787f6a6329a
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-09-23 12:31:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "thorough-vivi-filter-bot-7cde26c6.koyeb.app/")
+URL = environ.get("URL", "")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
