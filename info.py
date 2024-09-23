dea3becbffdb9a8279576f005fd059566d54fb86
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-09-23 16:30:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://efficient-swallow-filter-bot-3127ccd5.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
